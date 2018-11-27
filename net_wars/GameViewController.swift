b2c4df6b7abda613a7dd50729eded75b693d8ccd
Estuardo Valenzuela <eiv10@humboldt.edu>
2018-11-27 11:28:08 -0800

    Added gui and gyroscope functionality

--- net_wars/GameViewController.swift
+++ net_wars/GameViewController.swift
@@ -1,121 +1,122 @@
-//
-//  GameViewController.swift
-//  net_wars
-//
-//  Created by Estuardo Valenzuela on 11/15/18.
-//  Copyright © 2018 Estuardo Valenzuela. All rights reserved.
-//
-
 import UIKit
-import QuartzCore
 import SceneKit
+import CoreMotion
 
 class GameViewController: UIViewController {
-
+    
+    let motionManager = CMMotionManager()
+    let cameraNode = SCNNode()
+    
+    @IBOutlet weak var sceneView: SCNView!
+    @IBOutlet weak var scene: SCNScene!
+    
+    var firebutton: UIButton!
+    var thruster: UISlider!
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.darkGray
-        scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // set the scene to the view
-        scnView.scene = scene
-        
-        // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
-        
-        // show statistics such as fps and timing information
-        scnView.showsStatistics = true
-        
-        // configure the view
-        scnView.backgroundColor = UIColor.black
-        
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+        UIDevice.current.setValue(value, forKey: "orientation")
+        //super.viewDidLoad()
+        setupScene()
+    
     }
     
-    @objc
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
+    func setupScene(){
         
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result = hitResults[0]
-            
-            // get its material
-            let material = result.node.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
+        // Set the scene
+        sceneView = self.view as! SCNView
+        sceneView.delegate = self
+        let scene = SCNScene(named: "art.scnassets/level.scn")
+        sceneView.scene = scene
+        
+        // Camera, ...
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3Make(0, 0, 0)
+        scene?.rootNode.addChildNode(cameraNode)
+        
+        guard motionManager.isDeviceMotionAvailable else {
+            fatalError("Device motion is not available")
         }
+        
+        /*
+        // Action
+        motionManager.deviceMotionUpdateInterval = 1.0 / 60.0
+        motionManager.startDeviceMotionUpdates(to: OperationQueue.main, withHandler: {
+            data , error in
+            
+            guard let data = data else { return }
+            
+            let attitude: CMAttitude = data.attitude
+            self.cameraNode.eulerAngles = SCNVector3Make(Float(attitude.roll - M_PI/2.0), Float(attitude.yaw), Float(attitude.pitch))
+            print(data.attitude)
+            
+        })
+         */
+        
+        // Fire button setup
+        let firebutton:UIButton = UIButton(frame: CGRect(x: sceneView.frame.height * 0.80, y: sceneView.frame.width * 0.35, width: 100, height: 100))
+            firebutton.backgroundColor = .gray
+            firebutton.setTitle("Fire", for: .normal)
+            firebutton.addTarget(self, action:#selector(self.buttonClicked), for: .touchUpInside)
+            sceneView.addSubview(firebutton)
+        
+        // Thruster setup
+        let thruster: UISlider = UISlider(frame: CGRect(x: sceneView.frame.height * 0.01, y: sceneView.frame.width * 0.50, width: 150, height: 20))
+            thruster.transform = CGAffineTransform(rotationAngle: CGFloat(-Double.pi / 2))
+            thruster.minimumValue = 0
+            thruster.maximumValue = 100
+            thruster.isContinuous = true
+            thruster.tintColor = UIColor.yellow
+            sceneView.addSubview(thruster)
+        
+        
+        
+        //let firebutton:UIButton = UIButton(frame: CGRect(x: sceneView.frame.height * 0.80, y: //sceneView.frame.width * 0.35, width: 100, height: 100))
+        
+        //let thruster: UISlider = UISlider(frame: CGRect(x: sceneView.frame.height * 0.05, y: //sceneView.frame.width * 0.45, width: 150, height: 20))
+    }
+    
+    @objc func buttonClicked() {
+        print("BAM!")
+    }
+    
+    func sliderValueDidChange(_ sender:UISlider!)
+    {
+        print("Slider value changed")
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscapeLeft
     }
     
     override var shouldAutorotate: Bool {
         return true
     }
     
-    override var prefersStatusBarHidden: Bool {
-        return true
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
 }
+
+extension GameViewController : SCNSceneRendererDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        
+        motionManager.deviceMotionUpdateInterval = 1.0 / 60.0
+        motionManager.startDeviceMotionUpdates(to: OperationQueue.main, withHandler: {
+            data , error in
+            
+            guard let data = data else { return }
+            
+            let attitude: CMAttitude = data.attitude
+            self.cameraNode.eulerAngles = SCNVector3Make(Float(attitude.roll - M_PI/2.0), Float(attitude.yaw), Float(attitude.pitch))
+            //print(data.attitude)
+            print("Height: " , self.self.sceneView.frame.height)
+            print("Width: " , self.sceneView.frame.width)
+
+        })
+    }
+}
+
